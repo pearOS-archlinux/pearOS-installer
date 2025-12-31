dd9f1f49e524e371ed71367755748a288c52b7ef
Alexandru Balan <alxb421@gmail.com>
2025-12-31 23:04:28 +0200

    refresh UI

--- system_install/frontend/app/js/engine.js
+++ system_install/frontend/app/js/engine.js
@@ -1,26 +1,142 @@
 function open_gparted() {
 const { exec } = require('child_process');
-exec('gparted', (err, stdout) => {
-    console.log(`${stdout}`);
+exec('gparted', (err, stdout, stderr) => {
+    if (err) {
+        console.error('Error opening GParted:', err.message);
+        alert('GParted could not be opened. Make sure it is installed.');
+        return;
+    }
+    // GParted opened successfully
 })
 }
 
 function open_browser() {
 const { exec } = require('child_process');
-exec('firefox', (err, stdout) => {
-    console.log(`${stdout}`);
-})
+// Try Pafari first, fallback to xdg-open or firefox
+exec('pafari', (err) => {
+    if (err) {
+        // Pafari not found, try xdg-open
+        exec('xdg-open http://www.google.com', (err2) => {
+            if (err2) {
+                // xdg-open failed, try firefox
+                exec('firefox', (err3) => {
+                    if (err3) {
+                        console.error('Error opening browser:', err3.message);
+                        alert('Could not open browser. Make sure a browser is installed.');
+                    }
+                });
+            }
+        });
+    }
+});
 }
 
 function open_packup() {
 const { exec } = require('child_process');
-exec('', (err, stdout) => {
-    console.log(`${stdout}`);
+// TODO: Implement packup restore functionality
+// For now, just show a message or open packup application if available
+exec('packup &', (err) => {
+    if (err) {
+        console.error('Packup not available:', err.message);
+        alert('Packup restore functionality is not yet implemented or Packup is not installed.');
+    }
 })
 }
 
 function open_installer() {
  location.href = "page_install.html";
+}
+
+// Menu checkbox functionality
+function initMenuCheckboxes() {
+  var checkboxes = document.querySelectorAll('.menu_checkbox');
+  
+  // Update visual state for all checkboxes on init
+  checkboxes.forEach(function(checkbox) {
+    updateMenuCheckboxState(checkbox);
+  });
+  
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+      // If this checkbox is checked, uncheck all others
+      if (this.checked) {
+        checkboxes.forEach(function(cb) {
+          if (cb !== this) {
+            cb.checked = false;
+            updateMenuCheckboxState(cb);
+          }
+        }.bind(this));
+      }
+      // Update visual state
+      updateMenuCheckboxState(this);
+    });
+  });
+}
+
+function updateMenuCheckboxState(checkbox) {
+  var label = checkbox.closest('.menu_checkbox_label');
+  if (checkbox.checked) {
+    label.classList.add('menu_checkbox_checked');
+  } else {
+    label.classList.remove('menu_checkbox_checked');
+  }
+}
+
+function handleMenuAction(action) {
+  // Uncheck all checkboxes first
+  var allCheckboxes = document.querySelectorAll('.menu_checkbox');
+  allCheckboxes.forEach(function(cb) {
+    cb.checked = false;
+    updateMenuCheckboxState(cb);
+  });
+  
+  // Check the selected checkbox
+  var checkbox = document.getElementById('menu_' + action);
+  if (checkbox) {
+    checkbox.checked = true;
+    updateMenuCheckboxState(checkbox);
+  }
+  
+  // Execute the action
+  switch(action) {
+    case 'packup':
+      open_packup();
+      break;
+    case 'installer':
+      open_installer();
+      break;
+    case 'browser':
+      open_browser();
+      break;
+    case 'gparted':
+      open_gparted();
+      break;
+  }
+}
+
+function handleMenuContinue() {
+  var checkedCheckbox = document.querySelector('.menu_checkbox:checked');
+  if (!checkedCheckbox) {
+    alert('Please select an option');
+    return;
+  }
+  
+  // Execute action for the selected item
+  var action = checkedCheckbox.value;
+  switch(action) {
+    case 'packup':
+      open_packup();
+      break;
+    case 'installer':
+      open_installer();
+      break;
+    case 'browser':
+      open_browser();
+      break;
+    case 'gparted':
+      open_gparted();
+      break;
+  }
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -34,6 +150,11 @@ var z=``;
 var diskname = "";
 var disksize ="";
 var eventListenerAdded = false;
+
+// Initialize Continue button as disabled
+setTimeout(function() {
+  updateContinueButtonState();
+}, 100);
 
 // Adaugă event listener o singură dată folosind event delegation
 function addDiskSelectionListener() {
@@ -50,6 +171,7 @@ function addDiskSelectionListener() {
 				if (diskInstallText && selectedDiskName) {
 					diskInstallText.textContent = 'pearOS NiceC0re will be installed on the disk "' + selectedDiskName + '":';
 				}
+				updateContinueButtonState();
 			}
 		});
 	}
@@ -59,6 +181,29 @@ exec('list_disk count', (err, numberofdisks) => {
 var sCount = `${numberofdisks}`
 count = parseInt(sCount);
 console.log("Available disks are:" + count);
+
+// If no disks found, add a fake NODISK entry
+if (count === 0 || isNaN(count) || count < 1) {
+	var fakeDisk = `
+	<li>
+	  <label class="label_for_disk">
+	    <input type="radio" id="disk0" name="disk" value="/dev/null" data-disk-name="NODISK">
+    		<img class="disk_logo" height=50px src="../../resources/disk.png"></img>
+    		<p id="label_disk0" class="disk_title"><b>NODISK</b></p>
+    		<p class="disk_title" style="font-size: 0.8em; color: #999;">/dev/null</p>
+    		<p class="disk_title" style="font-size: 0.8em; color: #999;">00.00GB</p>
+	  </label>
+	</li>
+	`;
+	z += fakeDisk;
+	document.getElementById("disk_list").innerHTML = z;
+	addDiskSelectionListener();
+	// Update button state after fake disk is added
+	setTimeout(function() {
+	  updateContinueButtonState();
+	}, 100);
+	return;
+}
 
 while (i < (count+1)) {
         console.log("THE VALUE IS " + i);
@@ -85,6 +230,10 @@ while (i < (count+1)) {
         });
         i++;
 	}
+	// Update button state after all disks are loaded
+	setTimeout(function() {
+	  updateContinueButtonState();
+	}, 500);
 	})
 }
 
@@ -101,7 +250,56 @@ function checkInternetConnection(callback) {
   });
 }
 
+// Update Continue button state based on disk selection
+function updateContinueButtonState() {
+  // Find the Continue button - it has onclick="select_disk()"
+  var allButtons = document.querySelectorAll('.install-button-agreement');
+  var continueButton = null;
+  for (var j = 0; j < allButtons.length; j++) {
+    if (allButtons[j].getAttribute('onclick') && allButtons[j].getAttribute('onclick').includes('select_disk')) {
+      continueButton = allButtons[j];
+      break;
+    }
+  }
+  
+  if (!continueButton) return;
+  
+  var radios = document.getElementsByName('disk');
+  var hasSelection = false;
+  var selectedDisk = null;
+  for (var i = 0; i < radios.length; i++) {
+    if (radios[i].checked) {
+      hasSelection = true;
+      selectedDisk = radios[i];
+      break;
+    }
+  }
+  
+  // Disable button if no selection, or if NODISK is selected
+  if (!hasSelection || (selectedDisk && (selectedDisk.value === '/dev/null' || selectedDisk.getAttribute('data-disk-name') === 'NODISK'))) {
+    continueButton.disabled = true;
+    continueButton.classList.add('disabled');
+  } else {
+    continueButton.disabled = false;
+    continueButton.classList.remove('disabled');
+  }
+}
+
 function select_disk() {
+  // Check if NODISK is selected - prevent installation
+  var radios = document.getElementsByName('disk');
+  for (var i = 0, length = radios.length; i < length; i++) {
+    if (radios[i].checked) {
+      var selectedValue = radios[i].value;
+      var selectedName = radios[i].getAttribute('data-disk-name');
+      if (selectedValue === '/dev/null' || selectedName === 'NODISK') {
+        alert('Cannot install on NODISK. Please select a valid disk.');
+        return;
+      }
+      break;
+    }
+  }
+  
   // Verifică conexiunea la internet înainte de a permite instalarea
   checkInternetConnection(function(hasInternet) {
     if (!hasInternet) {
@@ -134,6 +332,35 @@ function print_disk(ctrl){
     //var TextInsideLi = ctrl.getElementsByTagName('p')[0].innerHTML;
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+// Mark selected option when language is selected
+function markSelectedLanguage() {
+  var select = document.getElementById("ddlViewBy");
+  if (select) {
+    // Remove selected class from all options
+    var options = select.getElementsByTagName('option');
+    for (var i = 0; i < options.length; i++) {
+      options[i].classList.remove('selected-option');
+    }
+    // Add selected class to current option
+    if (select.selectedIndex >= 0) {
+      options[select.selectedIndex].classList.add('selected-option');
+    }
+  }
+}
+
+// Initialize on page load
+window.addEventListener('load', function() {
+  var select = document.getElementById("ddlViewBy");
+  if (select) {
+    // Mark initial selection
+    markSelectedLanguage();
+    // Mark selection when changed
+    select.addEventListener('change', markSelectedLanguage);
+    // Mark selection when focus is lost
+    select.addEventListener('blur', markSelectedLanguage);
+  }
+});
+
 function select_language() {
   var e = document.getElementById("ddlViewBy");
   var strUser = e.options[e.selectedIndex].text;
@@ -145,4 +372,24 @@ function select_language() {
     else if (strUser == "Czech") {
       window.location.href='lg/cs/page_examining.html';
     }
+}
+
+// Agreement modal functions
+function showAgreementModal() {
+  var modal = document.getElementById('agreement-modal');
+  if (modal) {
+    modal.classList.add('show');
+  }
+}
+
+function closeAgreementModal() {
+  var modal = document.getElementById('agreement-modal');
+  if (modal) {
+    modal.classList.remove('show');
+  }
+}
+
+function confirmAgreement() {
+  closeAgreementModal();
+  window.location.href='page_install_disk.html';
 }
