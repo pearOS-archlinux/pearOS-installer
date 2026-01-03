9d39c344247978e305e515ae6856a5fbc7c0f0fa
Alexandru Balan <alxb421@gmail.com>
2026-01-04 01:10:58 +0200

    preliminary for 26.1

--- system_install/frontend/main.js
+++ system_install/frontend/main.js
@@ -43,7 +43,7 @@ function createWindow () {
   })
 
   // !! UNCOMMENT ONLY ON DEBUG !!
-   mainWindow.webContents.openDevTools()
+//   mainWindow.webContents.openDevTools()
 
   mainWindow.on('closed', function () {
     // Oprește power save blocker când fereastra se închide
